bbe5507ca11861ff73b6fa1ac92a1628f443c350
Avinash Kalwale <avinashkalwale12@gmail.com>
2022-12-26 22:54:09 +0530

    file added with code

---
+++ src/models/Students.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose')
+const {Schema}=mongoose;
+const studentSchema=new Schema({
+    name:String,
+    classId:{
+        type: Schema.Types.ObjectId,
+        ref:"Class"
+    }
+})
+const Student= mongoose.model("Student", studentSchema)
